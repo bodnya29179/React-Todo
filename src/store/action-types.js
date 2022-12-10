d221892cd092ea3e89d377d855a06c1d568a5e48
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-10 14:40:05 +0200

    Add action types

---
+++ src/store/action-types.js
@@ -0,0 +1,8 @@
+export const ACTION_TYPES = Object.freeze({
+  addTodo: '[Todo] Add todo',
+  openTodo: '[Todo] Open todo',
+  completeTodo: '[Todo] Complete todo',
+  inProgressToDo: '[Todo] Move todo to in-progress',
+  editTodo: '[Todo] Edit todo',
+  deleteTodo: '[Todo] Delete todo',
+});
